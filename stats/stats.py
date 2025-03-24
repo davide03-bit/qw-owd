9f443634a2cdf4463e7da74860fffee24bd04650
davide03-bit <davidedgstn@gmail.com>
2025-03-24 10:38:56 +0000

    correzione

--- stats/stats.py
+++ stats/stats.py
@@ -538,7 +538,7 @@ def print_summary_metrics(metrics):
     print(f"{'Duration:':30s}{tx_time_str:>15s}")
     print("------------------------------------------------------------")
 
-    print(f"{avg_bw_str}, {std_bw_str}, {throughput_mbps}, {goodput_mbps}, {avg_cwnd_kbit},{std_cwnd_kbit}, {avg_rtt_ms}, {std_rtt_ms}, {loss_rate_percent}, {retransmissions}, {total_time_s}")
+    print(f"{avg_bw_str}, {std_bw_str}, {throughput_str}, {goodput_str}, {avg_cwnd_str},{std_cwnd_str}, {avg_rtt_str}, {std_rtt_std}, {loss_rate_str}, {retransmissions}, {tx_time_str}")
 
 
 ###############################################################################
