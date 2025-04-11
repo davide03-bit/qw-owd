a01ee7398c7d6312f40208364e09b19d83a3a7c9
davide03-bit <davidedgstn@gmail.com>
2025-04-11 17:10:10 +0000

    fix

--- stats/grafici.sh
+++ stats/grafici.sh
@@ -3,6 +3,6 @@ sub=$1
 for i in {0..39}
 do
 file=$(ls -t /qw/qw/server/logs/$1 | head -n $((i+1)) | tail -1)
-python3 /qw/qw/stats.py --output $sub_$i.png /qw/qw/server/logs/$sub/$file
+python3 /qw/qw/stats/stats.py --output $sub_$i.png /qw/qw/server/logs/$sub/$file
 done
 
