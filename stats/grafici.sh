5a45d33294ea8cf9181aa818975dd44a2cb75bf6
davide03-bit <davidedgstn@gmail.com>
2025-04-11 17:06:30 +0000

    grafici

---
+++ stats/grafici.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sub=$1
+for i in {0..39}
+do
+file=$(ls -t /qw/qw/server/logs/$1 | head -n $((i+1)) | tail -1)
+python3 /qw/qw/stats.py --output $sub_$i.png /qw/qw/server/logs/$sub/$file
+done
+
