f290fcb3a3ddf5ff046806a535252531f347e5be
davide03-bit <davidedgstn@gmail.com>
2025-09-12 14:17:22 +0000

    ripristino reazione

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
@@ -260,22 +260,23 @@ void WestwoodOWD::onPacketAcked(
   // If the delay condition is met, adjust ssthresh and cwnd.
   if (delayControl(quicConnectionState_.transportSettings.ccaConfig
                       .delayControlFraction)) {
-    //uint64_t rttMinUs = rttSampler_.minRtt().count();
-    //ssthresh_ = std::max(
-        //static_cast<uint64_t>(bandwidthEstimate_ * (rttMinUs / 1e6)),
-        //2 * quicConnectionState_.udpSendPacketLen);
+    uint64_t rttMinUs = rttSampler_.minRtt().count();
+    ssthresh_ = std::max(
+        static_cast<uint64_t>(bandwidthEstimate_ * (rttMinUs / 1e6)),
+        2 * quicConnectionState_.udpSendPacketLen);
     // uint64_t owdth = quicConnectionState_.transportSettings.ccaConfig
     //                   .delayControlFraction * (lossMaxRtt_.count() - rttMinUs);
     // cwndBytes_ = std::max(
     //     static_cast<uint64_t>(bandwidthEstimate_ * ((rttMinUs + owdth) / 1e6)),
     //     2 * quicConnectionState_.udpSendPacketLen);
-    cwndBytes_ -= quicConnectionState_.udpSendPacketLen;
+    //cwndBytes_ -= quicConnectionState_.udpSendPacketLen;
+    cwndBytes_ = ssthresh_;
     cwndBytes_ = boundedCwnd(
         cwndBytes_,
         quicConnectionState_.udpSendPacketLen,
         quicConnectionState_.transportSettings.maxCwndInMss,
         quicConnectionState_.transportSettings.minCwndInMss);
-    ssthresh_ = cwndBytes_;
+    //ssthresh_ = cwndBytes_;
   }
 
   VLOG(10) << __func__ << " delay control triggered, ssthresh=" << ssthresh_
