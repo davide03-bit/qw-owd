7db36c8b5b1a73242ca47b1ae122d454614fc1bd
davide03-bit <davidedgstn@gmail.com>
2025-09-30 10:26:46 +0000

    fix

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
@@ -243,7 +243,7 @@ void WestwoodOWD::updateOneWayDelay(
   }
 
   std::cout << time_owd_us << " " << owd_ << " " << owdvCorrect_ << " "
-            << OneWayDelayMax_ << " " << " " << kRttMax << " " << krttMax - rttSampler_.minRtt().count() << std::endl;
+            << OneWayDelayMax_ << " " << " " << kRttMax << " " << kRttMax - rttSampler_.minRtt().count() << std::endl;
 }
 
 void WestwoodOWD::onPacketAcked(
